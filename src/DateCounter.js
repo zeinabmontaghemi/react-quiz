1fd892f97be7ae117b330a0f43ca58508b1656dd
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-07-30 20:32:43 +0330

    change some reduser's payload

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,8 +2,8 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + action.payload;
-  if (action.type === "dec") return state - action.payload;
+  if (action.type === "inc") return state + 1;
+  if (action.type === "dec") return state - 1;
   if (action.type === "setCount") return action.payload;
 }
 
@@ -17,13 +17,13 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch({ type: "dec", payload: -1 });
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch({ type: "inc", payload: 1 });
+    dispatch({ type: "inc" });
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
