11a0eb225789a9c80de1f9facf2d99bd6a8a0fbc
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-08-28 01:58:14 +0330

    change question.js

--- netlify/functions/questions.js
+++ netlify/functions/questions.js
@@ -3,6 +3,10 @@ import questions from "../../src/data/questions.json";
 export async function handler(event, context) {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+    },
     body: JSON.stringify(questions),
   };
 }
