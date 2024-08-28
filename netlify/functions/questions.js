52a7e2ecfdbd1323f52c95f712d6e605b7167fe3
zeinab montaghemi <zeinab.montaghemi@gmail.com>
2024-08-28 17:50:59 +0330

    change return valu of question.js

--- netlify/functions/questions.js
+++ netlify/functions/questions.js
@@ -3,6 +3,6 @@ import questions from "../../src/data/questions.json";
 export async function handler(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify(questions),
+    body: JSON.stringify({ questions }),
   };
 }
